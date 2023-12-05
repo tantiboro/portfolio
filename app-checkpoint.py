da1e0d86970e7338f3e74e731a8654adfacd71ec
Tanti Ouattara <52196770+tantiboro@users.noreply.github.com>
2023-12-04 23:10:30 -0600

    Add files via upload

---
+++ app-checkpoint.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+import requests
+ 
+st.title("Streamlit Components Examples")
+st.write(
+    """This app contains examples of 
+    Streamlit components, find them 
+    all in the sidebar!"""
+)
+ 
